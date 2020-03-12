5916784e1b206515ba738a076ab6988df1475c9d
Pierrick Delage <pierrick.delage@gmail.com>
2020-03-12 17:37:07 +0100

    trying flip events

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -3,3 +3,15 @@ require("@rails/activestorage").start()
 require("channels")
 
 import "bootstrap";
+
+const cardOver = document.querySelectorAll('.cardFront');
+
+cardOver.forEach((card) => {
+	card.addEventListener('mouseover', (event) => {
+		console.log(event);
+ 		event.target.innerText = `<p>Distance: <%= parcour.distance %> kilometres</p>
+							<p>Durée: <%= parcour.duree %></p>
+							<p>Denivele: <%= parcour.denivele %></p>
+							<p>Type: <%= parcour.difficulte %></p>`;
+	});
+});
