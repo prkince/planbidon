a7bad8ed63480e08bedf0752524d945a8b8146b4
Pierrick Delage <pierrick.delage@gmail.com>
2020-03-20 15:39:57 +0100

    work on css links and remove console.log

--- app/javascript/components/strava.js
+++ app/javascript/components/strava.js
@@ -8,24 +8,23 @@ let gaCredentials = [process.env.CLIENT_ID_GA, process.env.CLIENT_SECRET_GA, pro
 
 let finalArr = [];
 
-// Load prk details
+// 2 Load prk details
 let arrayPrk = [];
 // adding new maps from strava directly 
 //arrayPrk.push('2409058705', '2264900813', '2766037346', '2763328555');
-
 const stravaIdsPrk = document.querySelector(".strava_ids_prk");
 getStravaIds(stravaIdsPrk, arrayPrk);
 reAuthorize(prkCredentials, arrayPrk);
 
-// Load ga details
+// 3 Load ga details
 let arrayGa = [];
 // adding new maps from strava directly 
 //arrayGa.push('2356262757', '2354166691', '1767601829', '1899780707');
-
 const stravaIdsGa = document.querySelector(".strava_ids_ga");
 getStravaIds(stravaIdsGa, arrayGa);
 reAuthorize(gaCredentials, arrayGa);
 
+// 4 Get id by strava_id
 let arrayParcoursTotal = []
 const parcoursTotal = document.querySelector(".parcourstotal");
 getIds(parcoursTotal, arrayParcoursTotal);
@@ -43,10 +42,11 @@ function getIds(ids, array){
     });
 }
 
+// 5 display everything
 const newCards = document.querySelector("#cardsPrk");
 setTimeout(function() { 
     for (let key in finalArr) {
-    console.log(finalArr[key]);
+    // console.log(finalArr[key]);
       newCards.insertAdjacentHTML("beforeend", finalArr[key]);
     }
 }, 2000);
@@ -89,7 +89,7 @@ function getActivity(response, array){
         fetch(activity_link)
             .then((response) => response.json())
             .then((data) => {
-                console.log(data); //=> to display all available data
+                // console.log(data); //=> to display all available data
                 const km = (data.distance *  0.001).toFixed(2);
                 const measuredTime = new Date(null);
                 measuredTime.setSeconds(data.moving_time)
@@ -102,7 +102,7 @@ function getActivity(response, array){
                     }
                 })
                 let parcours = `
-                    <a href="/parcours/${answer}">
+                    <a href="/parcours/${answer}" style="text-decoration: none">
                     <div class="card-category-prk-2">
                         <div class="cards-details">
                             <div class="cards-headers">
